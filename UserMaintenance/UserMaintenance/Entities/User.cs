95f3b62becfe619c9d13e68f914f35c2c5f5ab55
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-09-26 21:37:12 +0200

    ötödik feladat

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -8,9 +8,9 @@ namespace UserMaintenance.Entities
 {
     class User
     {
-        public Guid ID { get; set } = Guid.NewGuid();
-        public string FirstName { get; set }
-        public string LastName { get; set }
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string FullName { 
             get 
             {
