61ec75fbd037eab0bde47a889e156f87e3550b09
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-09-26 23:10:12 +0200

    Fájlba írás

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaintenance.Entities;
 using System.Resources;
+using System.IO;
 
 namespace UserMaintenance
 {
@@ -19,19 +20,51 @@ namespace UserMaintenance
         {
             InitializeComponent();
             label1.Text = Resource1.FullName; 
-            label2.Text = Resource1.FullName; 
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             //listbox
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
 
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             var u = new User()
             {
-                FullName = textBox1.Text,
+                FullName = textBox1.Text
             };
             users.Add(u);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog mentes = new SaveFileDialog();
+            mentes.InitialDirectory = Application.StartupPath;
+            mentes.Filter = "Comma Seperated Values (*.csv) | *.csv";
+            mentes.DefaultExt = "csv";
+            mentes.AddExtension = true;
+
+            if (mentes.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(mentes.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.WriteLine(u.FullName);
+                    sw.Write(";");
+                    sw.WriteLine(u.ID);
+                    sw.Write(";");
+                }
+            }
+
+
+        }
+
+   
     }
 }
