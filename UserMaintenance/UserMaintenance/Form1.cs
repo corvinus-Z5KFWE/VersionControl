a9a55f11e12ca252d60a0a62dd687875052a648e
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-10-05 10:55:55 +0200

    8# változtatások

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -139,8 +139,28 @@ namespace UserMaintenance
 
             for (int i = 0; i < headers.Length; i++)
             {
-                
+                //első sorba kiiratás
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+
             }
         }
+
+        
     }
 }
