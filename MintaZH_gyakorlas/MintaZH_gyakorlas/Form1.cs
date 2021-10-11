15178f508a06af4a3b75065aee3ec0f7c8e6ff3f
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-10-11 18:28:31 +0200

    Adatbetölté

--- MintaZH_gyakorlas/MintaZH_gyakorlas/Form1.cs
+++ MintaZH_gyakorlas/MintaZH_gyakorlas/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,37 @@ namespace MintaZH_gyakorlas
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();
         public Form1()
         {
             InitializeComponent();
+            LoadData("Summer_olympic_Medals.csv");
+        }
+
+        private void LoadData(string practiceFile)
+        {
+            using (var sr = new StreamReader(practiceFile, Encoding.Default))
+            {
+                sr.ReadLine(); // első sor kimarad
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    var or = new OlympicResult()
+                    {
+                        Year = int.Parse(line[0]),
+                        Country = line[3],
+                        Medals = new int[]
+                        {
+                            int.Parse(line[5]),
+                            int.Parse(line[6]),
+                            int.Parse(line[7])
+                        }
+                    };
+
+                    results.Add(or);
+                }
+           }
+
         }
     }
 }
