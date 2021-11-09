e03b4aecbdef922f3d5befe89278eced37dd6040
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-11-09 13:21:14 +0100

    Futószalag létrehozása

--- Fejlesztési minták/Fejlesztési minták/Form1.cs
+++ Fejlesztési minták/Fejlesztési minták/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fejlesztési_minták.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,49 @@ using System.Windows.Forms;
 
 namespace Fejlesztési_minták
 {
-    public partial class Form1 : Form
+    public partial class form1 : Form
     {
-        public Form1()
+        private List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+        private BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+        public form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosiion = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosiion)
+                {
+                    maxPosiion = ball.Left;
+                }
+            }
+
+            if (maxPosiion >1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+
+        
     }
 }
