07dfe577676b5fc473bb934d95047c9a533d78b2
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-11-24 16:14:05 +0100

    E-mail validálás teszt függvény

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestvalidateEmail(string email, bool expectedResults)
+        {
+
+        }
+    }
+}
