baa1b2712e7ac5b452b901e3236e06b71e27eb0f
corvinus-Z5KFWE <gyetimarci2@gmail.com>
2021-11-24 16:17:56 +0100

    E-mail validálás teszt kifejtése

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
@@ -12,7 +13,14 @@ namespace UnitTestExample.Test
         [Test]
         public void TestvalidateEmail(string email, bool expectedResults)
         {
+            // Arrange
+            var accountController = new AccountController();
 
+            //Act
+            var acrualResult = accountController.ValidateEmail(email);
+
+            //Assert
+            Assert.AreEqual(expectedResults, acrualResult);
         }
     }
 }
